eeede4dd3a9ff8b270426947beca64c5bd45ccd3
Ninjinkai <nickpetty@hotmail.com>
2015-09-20 23:15:20 -0400

    Updated HW4.

--- hw4/js/calculator.js
+++ hw4/js/calculator.js
@@ -1,15 +1,18 @@
 //Calculate compound interest and output result.
-var result = function (principle, intRate, period) {
-    for (var i = 0; i < period; i++) {
-        principle += (principle * (intRate * 0.01));
+var result = function (principal, intRate, period) {
+    'use strict';
+    var i;
+    for (i = 0; i < period; i = i + 1) {
+//Typecast everything!
+        principal = Number(principal) + (Number(principal) * (Number(intRate) * 0.01));
     }
-    document.getElementById("result").innerHTML = "Total: $" + principle;
+//Dollar formatting from http://stackoverflow.com/questions/149055/how-can-i-format-numbers-as-money-in-javascript
+    document.getElementById("result").innerHTML = "Total: $" + principal.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
 };
 
 //Get values from fields and run calculate function when button is clicked.
 document.getElementById("calculate").onclick = function () {
-    var principle = document.getElementById("principle").value;
-    var intRate = document.getElementById("intRate").value;
-    var period = document.getElementById("period").value;
-    result(principle, intRate, period);
+    'use strict';
+    var principal = document.getElementById("principal").value, intRate = document.getElementById("intRate").value, period = document.getElementById("period").value;
+    result(principal, intRate, period);
 };
